7f1c4deda41a4452282dbd77bc31c5db573600d7
Chris_Bo <krzysztofbogusz050591@gmail.com>
2017-09-25 19:04:20 +0200

    add controling

--- javascript.js
+++ javascript.js
@@ -1,3 +1,11 @@
+// notes to my self
+// need to set up win function when and who wins the game?
+// - need to figure out how to look for patterns of win. 
+//- need to by comparing arrays agein or any other posibitly;
+// need to write logic for computer moves
+//
+
+
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
@@ -15,12 +23,10 @@ const ch = canvas.height;
 var player = "cros";
 var computer = "circle";
 
-
+var cliked = false;
 //cordinats of the circle
 
-var pos1X = 450;
-var pos1Y = 455;
-//I(90,90),II(80, 270), III(80, 450); II row(80, 270, 450)
+
 
 //cordinates of the x middle X 350, Y 190 
 /*
@@ -116,6 +122,48 @@ function eks(x, y) {
         var Y = 10;
         var X2 = 190;
         var Y2 = 360;
+    } else if (y <= 171 && x <= 530) {
+        //IV pozycja
+        var X = 170;
+        var Y = 10;
+        var X2 = 530;
+        var Y2 = 370;
+    } else if (y <= 350 && x <= 171) {
+        // II pozycja
+        var X = 350;
+        var Y = 190;
+        var X2 = 170;
+        var Y2 = 20;
+    } else if (y <= 350 && x <= 370) {
+        //V pozycja
+        var X = 350;
+        var Y = 190;
+        var X2 = 360;
+        var Y2 = 190;
+    } else if (y <= 350 && x <= 530) {
+        //VIII pozycja
+        var X = 350;
+        var Y = 190;
+        var X2 = 530;
+        var Y2 = 370;
+    } else if (y <= 530 && x <= 171) {
+        // III pozycja
+        var X = 530;
+        var Y = 370;
+        var X2 = 170;
+        var Y2 = 20;
+    } else if (y <= 530 && x <= 370) {
+        //VI pozycja
+        var X = 530;
+        var Y = 370;
+        var X2 = 360;
+        var Y2 = 190;
+    } else if (y <= 530 && x <= 530) {
+        //X pozycja
+        var X = 530;
+        var Y = 370;
+        var X2 = 530;
+        var Y2 = 370;
     } else {
         return false;
     }
@@ -130,8 +178,49 @@ function eks(x, y) {
 
 
 
+function ring(x, y) {
 
-function ring(corX, corY) {
+
+    if (y <= 171 && x <= 171) {
+        // I pozycja
+        var corX = 90;
+        var corY = 90;
+    } else if (y <= 171 && x <= 370) {
+        //IV pozycja
+        var corX = 270;
+        var corY = 90;
+    } else if (y <= 171 && x <= 530) {
+        //IV pozycja
+        var corX = 450;
+        var corY = 90;
+    } else if (y <= 350 && x <= 171) {
+        // II pozycja
+        var corX = 90;
+        var corY = 270;
+    } else if (y <= 350 && x <= 370) {
+        //V pozycja
+        var corX = 270;
+        var corY = 270;
+    } else if (y <= 350 && x <= 530) {
+        //VIII pozycja
+        var corX = 450;
+        var corY = 270;
+    } else if (y <= 530 && x <= 171) {
+        // III pozycja
+        var corX = 90;
+        var corY = 450;
+    } else if (y <= 530 && x <= 370) {
+        //VI pozycja
+        var corX = 270;
+        var corY = 450;
+    } else if (y <= 530 && x <= 530) {
+        //X pozycja
+        var corX = 450;
+        var corY = 450;
+    } else {
+        return false;
+    }
+
 
     ctx.beginPath();
     //ctx.arc(90/X, 90/Ycoordinate, 70/radius, 0, 2 * Math.PI);
@@ -139,16 +228,30 @@ function ring(corX, corY) {
     ctx.stroke();
     ctx.strokeStyle = "black";
 }
+
+//start writing the logic;
+function computerMoves() {
+
+}
 //mousemove to get cors while moving mouse; get pos on click
 function playerClick() {
     canvas.addEventListener('click', function (evt) {
         var mousePos = getMousePos(canvas, evt);
         var message = 'Mouse position: ' + mousePos.x + ' :X' + ',' + mousePos.y + ' :Y';
 
-        eks(mousePos.x, mousePos.y)
+        if (player === "cros" && !cliked) {
+            eks(mousePos.x, mousePos.y)
+            cliked = true;
+        } else if (!cliked) {
+            ring(mousePos.x, mousePos.y);
+        }
         console.log(message);
     }, false);
+
+
+
 }
+
 
 //gets mouse position
 function getMousePos(canvas, evt) {
@@ -162,6 +265,6 @@ function getMousePos(canvas, evt) {
 //draws a game
 function game() {
     lines();
-    ring(pos1X, pos1Y);
     playerClick();
+
 }
