b6a0c21e23580a77d5f371c2547116f836abe4e4
Chris_Bo <krzysztofbogusz050591@gmail.com>
2017-09-26 16:31:39 +0200

    add victory func and memory

--- javascript.js
+++ javascript.js
@@ -5,9 +5,8 @@
 // need to write logic for computer moves
 //
 
-
-const canvas = document.getElementById('myCanvas');
-const ctx = canvas.getContext('2d');
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
 
 canvas.style.backgroundColor = "lightgrey";
 canvas.width = 540;
@@ -16,16 +15,15 @@ canvas.height = 540;
 const cw = canvas.width;
 const ch = canvas.height;
 
-
-
-
-
 var player = "cros";
 var computer = "circle";
 
+//making sure player moves only onece
 var cliked = false;
-//cordinats of the circle
 
+//saves all player and computer movments in memry;
+var crosCordsMemo = [];
+var circleCordsMemo = [];
 
 
 //cordinates of the x middle X 350, Y 190 
@@ -93,6 +91,45 @@ IX ( X,X2 = 530; Y,Y2 = 370)
 //sets game timmig; 
 setInterval(game, 1000 / 60);
 
+//will look trough arrays for pattern maching if any of winng options will exisit it will pass mesege;
+function victoryOptions(arr) {
+
+    var winner = "";
+
+    if (arr === crosCordsMemo) {
+        winner = "CROS";
+    } else {
+        winner = "CIRCLE";
+    }
+
+    if (arr.indexOf("I") >= 0 && arr.indexOf("II") >= 0 && arr.indexOf("III") >= 0 ||
+        arr.indexOf("IV") >= 0 && arr.indexOf("V") >= 0 && arr.indexOf("VI") >= 0 ||
+        arr.indexOf("VII") >= 0 && arr.indexOf("VIII") >= 0 && arr.indexOf("IX") >= 0 ||
+        arr.indexOf("I") >= 0 && arr.indexOf("IV") >= 0 && arr.indexOf("VII") >= 0 ||
+        arr.indexOf("II") >= 0 && arr.indexOf("V") >= 0 && arr.indexOf("VIII") >= 0 ||
+        arr.indexOf("III") >= 0 && arr.indexOf("VI") >= 0 && arr.indexOf("IV") >= 0 ||
+        arr.indexOf("I") >= 0 && arr.indexOf("V") >= 0 && arr.indexOf("IX") >= 0 ||
+        arr.indexOf("VII") >= 0 && arr.indexOf("V") >= 0 && arr.indexOf("III") >= 0) {
+        alert(winner + " " + "wins the game!");
+        crosCordsMemo = [];
+        circleCordsMemo = [];
+        clearAll();
+
+    }
+
+}
+
+//function for clearing up the board!
+
+function clearAll() {
+
+}
+
+//start writing the logic;
+function computerMoves() {
+
+}
+
 function lines() {
     //lines Y
     ctx.fillStyle = 'green'
@@ -111,55 +148,55 @@ function lines() {
 function eks(x, y) {
 
     if (y <= 171 && x <= 171) {
-        // I pozycja
+        crosCordsMemo.push("I");
         var X = 170;
         var Y = 10;
         var X2 = 170;
         var Y2 = 10;
     } else if (y <= 171 && x <= 370) {
-        //IV pozycja
+        crosCordsMemo.push("IV");
         var X = 170;
         var Y = 10;
         var X2 = 190;
         var Y2 = 360;
     } else if (y <= 171 && x <= 530) {
-        //IV pozycja
+        crosCordsMemo.push("VII");
         var X = 170;
         var Y = 10;
         var X2 = 530;
         var Y2 = 370;
     } else if (y <= 350 && x <= 171) {
-        // II pozycja
+        crosCordsMemo.push("II");
         var X = 350;
         var Y = 190;
         var X2 = 170;
         var Y2 = 20;
     } else if (y <= 350 && x <= 370) {
-        //V pozycja
+        crosCordsMemo.push("V");
         var X = 350;
         var Y = 190;
         var X2 = 360;
         var Y2 = 190;
     } else if (y <= 350 && x <= 530) {
-        //VIII pozycja
+        crosCordsMemo.push("VIII");
         var X = 350;
         var Y = 190;
         var X2 = 530;
         var Y2 = 370;
     } else if (y <= 530 && x <= 171) {
-        // III pozycja
+        crosCordsMemo.push("III");
         var X = 530;
         var Y = 370;
         var X2 = 170;
         var Y2 = 20;
     } else if (y <= 530 && x <= 370) {
-        //VI pozycja
+        crosCordsMemo.push("VI");
         var X = 530;
         var Y = 370;
         var X2 = 360;
         var Y2 = 190;
     } else if (y <= 530 && x <= 530) {
-        //X pozycja
+        crosCordsMemo.push("IX");
         var X = 530;
         var Y = 370;
         var X2 = 530;
@@ -176,45 +213,51 @@ function eks(x, y) {
 
 }
 
-
-
 function ring(x, y) {
 
 
     if (y <= 171 && x <= 171) {
-        // I pozycja
+        circleCordsMemo.push("I");
         var corX = 90;
         var corY = 90;
     } else if (y <= 171 && x <= 370) {
         //IV pozycja
+        circleCordsMemo.push("IV");
         var corX = 270;
         var corY = 90;
     } else if (y <= 171 && x <= 530) {
-        //IV pozycja
+        //VII pozycja
+        circleCordsMemo.push("VII");
         var corX = 450;
         var corY = 90;
     } else if (y <= 350 && x <= 171) {
         // II pozycja
+        circleCordsMemo.push("II");
         var corX = 90;
         var corY = 270;
     } else if (y <= 350 && x <= 370) {
         //V pozycja
+        circleCordsMemo.push("V");
         var corX = 270;
         var corY = 270;
     } else if (y <= 350 && x <= 530) {
         //VIII pozycja
+        circleCordsMemo.push("VIII");
         var corX = 450;
         var corY = 270;
     } else if (y <= 530 && x <= 171) {
         // III pozycja
+        circleCordsMemo.push("III");
         var corX = 90;
         var corY = 450;
     } else if (y <= 530 && x <= 370) {
         //VI pozycja
+        circleCordsMemo.push("VI");
         var corX = 270;
         var corY = 450;
     } else if (y <= 530 && x <= 530) {
-        //X pozycja
+        //IX pozycja
+        circleCordsMemo.push("IX");
         var corX = 450;
         var corY = 450;
     } else {
@@ -229,10 +272,15 @@ function ring(x, y) {
     ctx.strokeStyle = "black";
 }
 
-//start writing the logic;
-function computerMoves() {
-
+//gets mouse position
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: Math.round((evt.clientX - rect.left) / (rect.right - rect.left) * canvas.width),
+        y: Math.round((evt.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height)
+    };
 }
+
 //mousemove to get cors while moving mouse; get pos on click
 function playerClick() {
     canvas.addEventListener('click', function (evt) {
@@ -241,6 +289,9 @@ function playerClick() {
 
         if (player === "cros" && !cliked) {
             eks(mousePos.x, mousePos.y)
+
+            console.log(crosCordsMemo);
+            //changes cliked so player can have only one move!
             cliked = true;
         } else if (!cliked) {
             ring(mousePos.x, mousePos.y);
@@ -252,19 +303,10 @@ function playerClick() {
 
 }
 
-
-//gets mouse position
-function getMousePos(canvas, evt) {
-    var rect = canvas.getBoundingClientRect();
-    return {
-        x: Math.round((evt.clientX - rect.left) / (rect.right - rect.left) * canvas.width),
-        y: Math.round((evt.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height)
-    };
-}
-
 //draws a game
 function game() {
     lines();
     playerClick();
-
+    victoryOptions(crosCordsMemo);
+    victoryOptions(circleCordsMemo);
 }
